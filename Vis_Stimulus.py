3314c910f3e494c4f685022c06ed6ac67034269c
Tawsif Ahmed <121985115+Tawzoo@users.noreply.github.com>
2024-07-30 14:12:49 -0700

    Changed the files

--- Vis_Stimulus.py
+++ Vis_Stimulus.py
@@ -2,12 +2,17 @@ import pygame # 2.6.0
 import sys # python version 
 import time # python version   
 import random # python version
+import os # python version
+from PIL import Image # 10.4.0
+import pyautogui # 0.9.54
+import threading # python version 
 
 pygame.init()
 
-# Background
+# Global variables
 global background_white 
 
+# Background
 background_white = random.choice([True, False])
 
 # Screen resolution
@@ -36,22 +41,27 @@ final_circle_gap = final_circle_width / 3
 screen = pygame.display.set_mode((width, height))
 
 # Function to draw the bars
-def bar_drawing(screen, bars):
+def bar_drawing(screen, bars, frame_number, folder, counter):
     draw_background()
 
     # For each bar, draw based on the color, position, current widhth and height 
     for bar in bars:
         pygame.draw.rect(screen, bar['color'], (*bar['pos'], bar['width'], bar['height']))
     pygame.display.flip()
+    # Save the animation by frames
+    # pygame.image.save(screen, f"{folder}/Animation/animation{counter}_frame{frame_number:04d}.png")
+
 
 # Function to draw circles
-def circle_drawing(screen, circles):
+def circle_drawing(screen, circles, frame_number, folder, counter):
     draw_background()
 
     # FOr each cycle, draw based on color, position and radius
     for circle in circles:
         pygame.draw.circle(screen, circle['color'], circle['pos'], circle['radius'])
     pygame.display.flip()
+    # Save the animation by frames
+    # pygame.image.save(screen, f"{folder}/Animation/animation{counter}_frame{frame_number:04d}.png")
 
 # Function to get background
 def draw_background():
@@ -62,10 +72,60 @@ def draw_background():
     # Half White & Half Gray if it is not 
     else:
         screen.fill(white)
-        pygame.draw.rect(screen,white, (0,0, width, height /2))
         pygame.draw.rect(screen, gray, (0, height /2, width, height /2))
+
+# Working on this 
+def capture_screen(folder,counter):
+    start_time = time.time()
+    frame_number = 0
+    duration = 10
+    interval = 0.000001
+    
+    while time.time() - start_time < duration:
+        screenshot = pyautogui.screenshot()
+        screenshot_path = os.path.join(f"{folder}/Screen", f"screenshot_{counter}_frame{frame_number:04d}.png")
+        screenshot.save(screenshot_path)
+        print("saved screnshot")
+        frame_number += 1
+        time.sleep(interval)
+
+# Working on this 
+def merge_images(folder, counter):
+
+    screenshots_folder = os.path.join(folder, "Screen")
+    animations_folder = os.path.join(folder, "Animation")
+    merge_folder = os.path.join(folder, "Merge")
+
+    screenshots = sorted(os.listdir(f"{folder}/Screen"))
+    animations = sorted(os.listdir(f"{folder}/Animation"))
+
+    total_count = min(len(screenshots), len(animations))
+
+    img_counter = 0
+
+    for i in range(total_count):
+
+        screenshot_path = os.path.join(screenshots_folder, screenshots[i])
+        animation_path = os.path.join(animations_folder, animations[i])
+
+        first_img = Image.open(screenshot_path)
+        second_img = Image.open(animation_path)
+
+        second_img = second_img.resize(first_img.size)
+
+        merge_img = Image.new('RGB', (first_img.width*2, first_img.height))
+        merge_img.paste(second_img, (0, 0))
+        merge_img.paste(first_img, (first_img.width, 0))
+
+        merge_path = os.path.join(merge_folder, f"merge_{counter}_{img_counter}.png")
+
+        merge_img.save(merge_path)
+        img_counter += 1
+    
+    print("Finished merging")
+
 # Function for two bar horizontal animation 
-def two_bars_hor_animation():
+def two_bars_hor_animation(folder, counter):
     
     # Randomizing the colors of two bars
     random.shuffle(colors_two)
@@ -74,10 +134,16 @@ def two_bars_hor_animation():
         {'color': colors_two[1], 'pos': [0,0], 'width': initial_width, 'height': initial_height}  
     ]
 
+    # Frames
+    frame_number = 0
+
     # Updating the screen with the white background 
     draw_background()
     pygame.display.flip()
     time.sleep(2)
+
+    # screenshot_thread = threading.Thread(target=capture_screen, args = (folder,counter))
+    # screenshot_thread.start()
 
     # Growing animation
     start_time = time.time()
@@ -98,7 +164,8 @@ def two_bars_hor_animation():
             bar['width'] = current_width
 
         # Drawing the bars 
-        bar_drawing(screen, bars)
+        bar_drawing(screen, bars, frame_number, folder, counter)
+        frame_number += 1
         pygame.time.delay(10)
     
     # Final bar positions
@@ -110,12 +177,12 @@ def two_bars_hor_animation():
     bars[0]["pos"][0] = (width / 2) - final_width - (final_gap / 2)
     bars[1]["pos"][0] = (width / 2) + (final_gap / 2)
 
-    bar_drawing(screen, bars)
+    bar_drawing(screen, bars, frame_number, folder, counter)
     time.sleep(1)
 
     # Moving off Screen animation 
     start_time = time.time()
-    while time.time() - start_time < 3:
+    while time.time() - start_time < 5:
         # Updating the position of the bars
         total_time = time.time() - start_time
         for bar in bars:
@@ -123,7 +190,8 @@ def two_bars_hor_animation():
                 bar['pos'][0] -= int( ( width / 2 + final_width + final_gap)/45 * (total_time / 10))
             else:
                 bar['pos'][0] += int( ( width / 2 + final_width + final_gap)/45 * (total_time / 10))
-        bar_drawing(screen, bars)
+        bar_drawing(screen, bars, frame_number, folder, counter)
+        frame_number += 1
         pygame.time.delay(10)
 
     # Updating the screen with the white background 
@@ -131,8 +199,10 @@ def two_bars_hor_animation():
     pygame.display.flip()
     time.sleep(2)
 
+    # screenshot_thread.join()
+
 # Function for three bar horizontal animation 
-def three_bars_hor_animation():
+def three_bars_hor_animation(folder, counter):
     
     # Randomizing the colors of three bars
     random.shuffle(colors_three)
@@ -142,10 +212,14 @@ def three_bars_hor_animation():
         {'color': colors_three[2], 'pos': [0,0], 'width': initial_width, 'height': initial_height}  
     ]
 
+    # Frames
+    frame_number = 0
+
     # Updating the screen with the white background 
     draw_background()
     pygame.display.flip()
     time.sleep(2)
+    
 
     # Growing animation
     start_time = time.time()
@@ -166,7 +240,8 @@ def three_bars_hor_animation():
             bar['width'] = current_width
 
         # Drawing the bars 
-        bar_drawing(screen, bars)
+        bar_drawing(screen, bars, frame_number, folder, counter)
+        frame_number += 1
         pygame.time.delay(10)
 
     # Final bar positions
@@ -180,12 +255,12 @@ def three_bars_hor_animation():
     bars[2]["pos"][0] = (width / 2) + (final_width / 2 + final_gap)
 
     # Holding the bar
-    bar_drawing(screen, bars)
+    bar_drawing(screen, bars, frame_number, folder, counter)
     time.sleep(1)
 
     # Moving off Screen animation 
     start_time = time.time()
-    while time.time() - start_time < 3:
+    while time.time() - start_time < 5:
         # Updating the position of the bars
         total_time = time.time() - start_time
         for bar in bars:
@@ -193,7 +268,8 @@ def three_bars_hor_animation():
                 bar['pos'][0] -= int( ( width / 2 + final_width + final_gap)/45 * (total_time / 10))
             elif bar == bars[2]:
                 bar['pos'][0] += int( ( width / 2 + final_width + final_gap)/45 * (total_time / 10))
-        bar_drawing(screen, bars)
+        bar_drawing(screen, bars, frame_number, folder, counter)
+        frame_number += 1
         pygame.time.delay(10)
     
     # Updating the screen with the white background 
@@ -202,7 +278,7 @@ def three_bars_hor_animation():
     time.sleep(2)
 
 # Function for two bar vertical animation 
-def circles_animation():
+def circles_animation(folder, counter):
 
     # Randomizing the colors of two circles
     random.shuffle(colors_two)
@@ -211,7 +287,9 @@ def circles_animation():
         {'color': colors_two[1], 'pos': [0,0], 'radius': initial_width / 2}
     ]
 
-    
+    # Frames
+    frame_number = 0
+   
     # Updating the screen with the white background 
     draw_background()
     pygame.display.flip()
@@ -233,7 +311,8 @@ def circles_animation():
             circle['radius'] = current_radius
 
         # Drawing the bars 
-        circle_drawing(screen, circles)
+        circle_drawing(screen, circles, frame_number, folder, counter)
+        frame_number += 1
         pygame.time.delay(20)
 
     # Holding animation
@@ -241,14 +320,12 @@ def circles_animation():
     for circle in circles:
         circle['pos'][1] = height /2
         circle['radius'] = final_circle_width / 2
-        print(circle['pos'][0])
    
     circles[0]['pos'][0] = width - final_circle_width  - final_circle_gap*2 - 35.5
-    print(final_circle_width, final_circle_gap, circles[0]['pos'][0], circles[0]['pos'][1] )
     circles[1]['pos'][0] = width - final_circle_width / 2 - final_circle_gap / 2 + 20
 
     draw_background()
-    circle_drawing(screen, circles)
+    circle_drawing(screen, circles, frame_number, folder, counter)
     pygame.display.flip()
     time.sleep(5)
 
@@ -259,7 +336,9 @@ def circles_animation():
 
 # Main function
 def main():
-    # Pressing 'b' starts the animation 
+    # Pressing 'b' starts the animation
+    folder = "C:/Users/tahme/OneDrive - UW/Research/Visual Stimulus"
+    counter = 0  
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -269,17 +348,20 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_b:
+                elif event.key == pygame.K_b:
                     # Randomly choice based an animation
                     # 1/4 chance for each animation
                     animation = random.choice([1, 2, 3])
+                    print("Choosing Animation")
                     if animation == 1:
-                        two_bars_hor_animation()
+                        two_bars_hor_animation(folder, counter)
                     elif animation == 2:
-                        three_bars_hor_animation()
+                        three_bars_hor_animation(folder, counter)
                     else:
-                        circles_animation()
+                        circles_animation(folder, counter)
+                    counter += 1
+                    print(counter)
+                # merge_images(folder, counter)
         draw_background()
         pygame.display.flip()
    
